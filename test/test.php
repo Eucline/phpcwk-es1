6ecc8ac60c9a993e705a101bdaeed590130bd0b0
Eucline Sweeney <P15229636@LEC-ADMIN.dmu.ac.uk>
2018-01-11 16:32:23 +0000

    Added test case file here.

---
+++ test/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p15229636
+ * Date: 11/01/2018
+ * Time: 12:02
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class test extends TestCase{
+    public function testUsername()
+    {
+        
+    }
+}
+

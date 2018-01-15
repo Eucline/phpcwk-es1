01746ad9d9ad84dc67e0d093e4232b29d10491af
Eucline <p15229636@my365.dmu.ac.uk>
2018-01-15 13:21:58 +0000

    Added a encoded and decoded test but don't know if it works.

--- test/test.php
+++ test/test.php
@@ -6,12 +6,32 @@
  * Time: 12:02
  */
 
+require_once('includes/test.php');
+require_once('simpletest/test/autorun.php');
+//require_once(dirname(__FILE__) . '/simpletest.php');
 use PHPUnit\Framework\TestCase;
 
-class test extends TestCase{
-    public function testUsername()
+final class test extends UnitTestCase
+{
+    public function testEncode()
     {
-        
+        $email = new userEmail();
+
+        $stringEncode = 'qwerty@email.com';
+        $encodedString = $email->encode64($stringEncode);
+        $expectedLength = 24;
+
+        $this->assertEqual($expectedLength, strlen($encodedString), $encodedString);
+    }
+
+    public function testDecode()
+    {
+        $email = new userEmail();
+
+        $stringEncode = 'qwerty@email.com';
+        $encodedString = $email->encode64($stringEncode);
+        $decodeString = $email->encode64($encodedString);
+
+        $this->assertEqual($stringEncode, $decodeString, decodeString);
     }
 }
-
